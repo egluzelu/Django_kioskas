b2888890f6ca79b19885b5bb46566a9e4a8e6a30
egluzelu <egluze.lu@gmail.com>
2024-02-20 09:01:41 +0200

    koreguotas help text

--- tasker_kioskas/user_profile/forms.py
+++ tasker_kioskas/user_profile/forms.py
@@ -1,8 +1,17 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import get_user_model
+from django.utils.translation import gettext_lazy as _
 
 
 class CreateUserForm(UserCreationForm):
     class Meta:
         model = get_user_model()
         fields = ('username', 'first_name', 'last_name', 'email')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['password1'].help_text=_('''
+            Your password can not be too similar to your other personal information,<br> 
+            Your password must contain at least 8 characters,<br> 
+            Your password can not be a commonly used,<br>
+            Your password can not be entirely numeric.''')
